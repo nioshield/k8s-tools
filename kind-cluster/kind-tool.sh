eb08124315856e6b8479ba30a6e179941f8083ad
nio <nioshield@gmail.com>
2023-10-13 10:42:08 +0800

    add chaso-mesh install/uninstall cmd

--- kind-cluster/kind-tool.sh
+++ kind-cluster/kind-tool.sh
@@ -19,11 +19,17 @@ Options:
        -iv,--imageVersion      version of the Load docker-images, default value: v2.6.2
        -in,--imageNames        name of the Load docker-images, use "," interval, default value: chaos-mesh,chaos-daemon
        -ir,--imageRegister     register of the Load docker-images, default value: ghcr.io/chaos-mesh
-       -la,--loadAllNodes      load docker-image to all nodes include controller node
+       -la,--loadAllNodes      load docker-image to all nodes include controller node, default false
+       -d,--chartDir           chaos-mesh helm chart dir, default /home/chaos-mesh
+       -hv,--helmValues        values string array for helm chart, default images.tag=v2.6.2,controllerManager.replicaCount=1,dashboard.create=false,dnsServer.create=false
+       -id,--ignoreDevPatch    ignore dev env cmd and args path for chaos-controller-manager, default false
 Actions:
       create                  create Kubernetes cluster
       destory                 delete Kubernetes cluster
       load                    load docker images to Kubernetes cluster
+      remote                  create dev cluster and a remoute cluster,load default docker image to cluster work node
+      destory_remote          destory dev and remote cluster
+      install_chaos           use Helm chart install chaos-mesh, replace conatiner cmd to dev mod, if set --skipDev the ignoer replace cmd action
 Usage:
     $0 carete --name testCluster --nodeNum 4 --k8sVersion v1.20.7
 EOF
@@ -121,6 +127,41 @@ destory_remote(){
     destory
 }
 
+install_chaos(){
+  log_info "############# install chaos-mesh:[${clusterName}] #############"
+  k8sConf=${ROOT}/kind/${clusterName}/k8s-config.yaml
+  if [ ! -f $k8sConf ]; then
+    log_error "k8s config ${k8sConf} is not exists, can not install chaos-mesh"
+    exit 1;
+  fi
+  if [ ! -d $chartDir ]; then
+    log_error "Helm chart ${chartDir} is not exits"
+    exit 1; 
+  fi
+  setValue=""
+  if [ -z "$helmValues" ];then
+      setValue="--set ${helmValue}"
+  fi
+  log_info `kubectl --kubeconfig ${k8sConf} create ns chaos-mesh`
+  helm --kubeconfig ${k8sConf} install chaos-mesh ${chartDir} -n chaos-mesh ${setValue}
+  
+  if [ "$ignoreDevPatch" == "false" ]; then
+    kubectl --kubeconfig ${k8sConf} patch deployment chaos-controller-manager -p '{"spec":{"template":{"spec":{"containers":[{"name":"chaos-mesh","command":["/bin/sh", "-c", "--"],"args":["while true; do sleep 30; done;"]}]}}}}' -n chaos-mesh 
+  fi
+  log_info "install chaos-mesh end"
+}
+
+uninstall_chaos(){
+  log_info "############# uninstall chaos-mesh:[${clusterName}] #############"
+  k8sConf=${ROOT}/kind/${clusterName}/k8s-config.yaml
+  if [ ! -f $k8sConf ]; then
+    log_error "k8s config ${k8sConf} is not exists, can not uninstall chaos-mesh"
+    exit 1;
+  fi
+  helm --kubeconfig ${k8sConf} uninstall chaos-mesh -n chaos-mesh 
+  log_info "uninstall chaos-mesh end"
+}
+
 apply_remote_k8sconf(){
   if [[ $# -lt 2 ]];then
       log_error "apply remote k8s config params num err"
@@ -148,6 +189,9 @@ info(){
   log_info "imageNames: ${imageNames}"
   log_info "imageRegister=${imageRegister}"
   log_info "loadAllNodes=${loadAllNodes}"
+  log_info "chartDir=${chartDir}"
+  log_info "helmValues=${helmValues}"
+  log_info "ignoreDevPatch=${ignoreDevPatch}"
 }
 
 log_info(){
@@ -212,6 +256,21 @@ case $key in
     shift
     shift
     ;;
+    -d|--chartDir)
+    chartDir="$2"
+    shift
+    shift
+    ;;
+    -hv|--helmValues)
+    chartDir="$2"
+    shift
+    shift
+    ;;
+    -id|--ignoerDevPatch)
+    ignoreDevPatch="$2"
+    shift
+    shift
+    ;;
    -h|--help)
     usage
     exit 0
@@ -231,6 +290,9 @@ imageVersion=${imageVersion:-v2.6.2}
 imageNames=${imageNames:-chaos-mesh,chaos-daemon}
 imageRegister=${imageRegister:-ghcr.io/chaos-mesh}
 loadAllNodes=${loadAllNodes:-false}
+chartDir=${chartDir:-/home/chaos-mesh/}
+helmValues=${helmValues:-images.tag=v2.6.2,controllerManager.replicaCount=1,dashboard.create=false,dnsServer.create=false}
+ignoreDevPatch=${ignoreDevPatch:-false}
 
 info
 
@@ -253,6 +315,14 @@ case $action in
     ;;
     destory_remote)
     destory_remote
+    exit 0
+    ;;
+    install_chaos)
+    install_chaos
+    exit 0
+    ;;
+    uninstall_chaos)
+    uninstall_chaos
     exit 0
     ;;
     *)
